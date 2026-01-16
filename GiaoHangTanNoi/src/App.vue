15804f6d255bc81a6adfa9cfc09cd883dd57cbb0
Hai Quan <haiquan2482006@gmail.com>
2026-01-16 16:20:09 +0700

    Cap nhat code moi nhat

--- GiaoHangTanNoi/src/App.vue
+++ GiaoHangTanNoi/src/App.vue
@@ -1,87 +1,61 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import './assets/base.css'
+import './assets/icon/icon/themify-icons-font/themify-icons-font/themify-icons/themify-icons.css'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div id="main">
+    <header>
+      <a href="#"><i>&#9776;</i></a>
+      <img src="./assets/anh.logo/anhnen.png" alt="">
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <a href=""><i class="icon-search ti-search"></i></a>
+      <a href=""><i class="icon-user ti-user"></i></a>
+    </header>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
-
-  <h1>xin chao</h1>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
   header {
+    width: 100%;           /* Trải dài hết chiều ngang */
+    height: 88px;
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    position: fixed;       /* Cố định ở trên cùng */
+    top: 0;                /* Sát mép trên */
+    left: 0;               /* Sát mép trái */
+    padding: 20px;         /* Tạo khoảng cách cho icon đỡ sát lề */
+    background-color: #00ff28;/* Thêm màu nền để dễ phân biệt (tùy chọn) */
+    z-index: 1000;         /* Đảm bảo luôn nằm trên các thành phần khác */
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+  header i{
+    font-size: 30px;
+    color: #FFFF;
+    padding-right: 10px;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  header img {
+    height: 164px;
+    margin-top: -60px;
+    margin-left: 20px;
   }
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+  header .icon-search,
+  header .icon-user{
+    color: #FFFF;
+    font-size: 20px;
+    text-decoration: none;
+    text-align: center;
   }
-}
+  
+  header a:last-child { 
+  margin-left: auto;     /* Tuyệt chiêu: Đẩy phần tử cuối cùng sang phải */
+  }
+
+  header a:last-child {
+    margin-left: auto;
+  }
 </style>
