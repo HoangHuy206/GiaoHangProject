4bd9ec385f89efce8171c417d03c0e21304135d8
Hai Quan <haiquan2482006@gmail.com>
2026-01-18 01:02:21 +0700

    update form tai xe

--- GiaoHangTanNoi/src/main.js
+++ GiaoHangTanNoi/src/main.js
@@ -3,7 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 
 // QUAN TRỌNG: Phải import từ App.vue (cái khung nhà) chứ KHÔNG import Login
-import App from './App.vue' 
+import App from './views/trangchutaixe.vue' 
 // import App from '../src/views/SanPham/main-SP.vue' 
  
 
